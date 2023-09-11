48d0ff17b4cf3c8d055486b01eafacb3c136a149
Ger Thao <g.thao19@gmail.com>
2023-09-11 16:25:18 -0500

    clean up some code in day one challenge.

--- src/main/scala/gerthao/adventofcode/DayOne.scala
+++ src/main/scala/gerthao/adventofcode/DayOne.scala
@@ -2,6 +2,7 @@ package gerthao.adventofcode
 
 import gerthao.adventofcode.PipeOps.*
 
+import scala.annotation.tailrec
 import scala.io.Source
 object DayOne:
   def main(args: Array[String]): Unit =
@@ -11,26 +12,25 @@ object DayOne:
       (_.sum) |>
       println
 
-  private def loadFoodGroupedByElf: List[List[String]] =
-    def extractValues(list: Iterator[String], acc: List[List[String]]): List[List[String]] =
+  private def loadFoodGroupedByElf: List[List[Int]] =
+    @tailrec
+    def extractValues(list: Iterator[String], acc: List[List[Int]] = List.empty): List[List[Int]] =
       val (left, right) = list.span(_.nonEmpty)
 
       if right.nonEmpty
-      then extractValues(right.drop(1), acc :+ (left.toList))
-      else acc :+ (left.toList)
+      then extractValues(right.drop(1), acc :+ left.map(_.toInt).toList)
+      else acc :+ left.map(_.toInt).toList
     end extractValues
 
     val reader = Source.fromFile("src/main/resources/ElfFoodCalorieList.txt")
-    val result = extractValues(reader.getLines(), List.empty)
+    val result = extractValues(reader.getLines)
 
-    reader.close()
+    reader.close
     result
   end loadFoodGroupedByElf
 
-  private def findSumOfCaloriesByElf(food: List[List[String]]): List[Int] =
-    food
-      .map(_.map(_.toInt))
-      .map(_.sum)
+  private def findSumOfCaloriesByElf(food: List[List[Int]]): List[Int] =
+    food.map(_.sum)
 
   private def takeHighestCaloriesByElf(times: Int)(list: List[Int]): List[Int] =
     list.sorted(Ordering.Int.reverse).take(times)
