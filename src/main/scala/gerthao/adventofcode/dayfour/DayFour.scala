31ca8e17d5040a60746fcbcc2f212f922d3c0db1
Ger Thao <g.thao19@gmail.com>
2023-09-16 11:38:50 -0500

    refactor code to load data in memory, change criteria to use iterable instead of iterator.

--- src/main/scala/gerthao/adventofcode/dayfour/DayFour.scala
+++ src/main/scala/gerthao/adventofcode/dayfour/DayFour.scala
@@ -1,16 +1,15 @@
 package gerthao.adventofcode.dayfour
 
 import gerthao.adventofcode.PipeOps.*
-import gerthao.adventofcode.FileHelper.*
+import gerthao.adventofcode.FileHelper
 
 object DayFour:
   type AssignmentPair          = (Assignment, Assignment)
-  type AssignmentCountCriteria = Iterator[AssignmentPair] => Int
+  type AssignmentCountCriteria = Iterable[AssignmentPair] => Int
 
   @main def run(): Unit =
-    val mainLogic = (criteria: AssignmentCountCriteria) => read("src/main/resources/dayfour/input.txt")(
-      _.getLines() |> parseData |> criteria
-    )
+    lazy val data = FileHelper.getLines("src/main/resources/dayfour/input.txt")
+    val mainLogic = data |> parseData |> (_: AssignmentCountCriteria)
 
     val firstCriteria: AssignmentCountCriteria  = _.count { case (first, second) => (first contains second) || (second contains first) }
     val secondCriteria: AssignmentCountCriteria = _.count { _ overlaps _ }
@@ -18,7 +17,7 @@ object DayFour:
     firstCriteria  |> mainLogic |> (result => println(s"[Part One] Assignment Count: $result"))
     secondCriteria |> mainLogic |> (result => println(s"[Part Two] Assignment Count: $result"))
 
-  private def parseData(data: Iterator[String]): Iterator[AssignmentPair] =
+  private def parseData(data: Iterable[String]): Iterable[AssignmentPair] =
     data
       .map { _.split(",") }
       .map { case Array(first, second) => (first.split("-"), second.split("-")) }
