ac95316f8f9017685621a99dcf4dbbb63b555fc1
Ger Thao <g.thao19@gmail.com>
2023-09-22 22:32:06 -0500

    small refactor for day six

--- src/main/scala/gerthao/adventofcode/daysix/DaySix.scala
+++ src/main/scala/gerthao/adventofcode/daysix/DaySix.scala
@@ -1,21 +1,19 @@
 package gerthao.adventofcode.daysix
 
 import gerthao.adventofcode.FileHelper
-import gerthao.adventofcode.PipeOps.|>
 
 object DaySix:
   @main def run(): Unit =
-    val startOfPacketMarker = getMarker(4)
+    val startOfPacketMarker  = getMarker(4)
     val startOfMessageMarker = getMarker(14)
     println(s"Start of packet marker result: $startOfPacketMarker")
     println(s"Start of packet message result: $startOfMessageMarker")
 
   private def getMarker(chunkSize: Int): Option[(Seq[Char], Int)] =
-    val markerCriteria: Seq[Char] => Boolean = l => l.length == l.toSet.size
     FileHelper.read("src/main/resources/daysix/input.txt"): source =>
       source.sliding(chunkSize)
         .zipWithIndex
-        .find { case (chunk, _) => chunk |> markerCriteria }
+        .find { case (chunk, _) => chunk.length == chunk.toSet.size }
         .map { case (chunk, index) => (chunk, index + chunk.length) }
 end DaySix
 
